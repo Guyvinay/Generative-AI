bfa26178e818cd8bdc4af19c0ffc52a239bed6b5
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-13 23:27:42 +0530

    ChatGpt-quote-app

---
+++ ChatGptQuoteApp/S1D1-ChatGptIntegration-QuoteApp/src/main/java/com/app/S1D1ChatGptIntegrationQuoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1D1ChatGptIntegrationQuoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1D1ChatGptIntegrationQuoteAppApplication.class, args);
+	}
+
+}
