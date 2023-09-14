c9e959a4f7b0be802cd8164c4384c0ee6da383c4
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-14 18:36:02 +0530

    python-basic-tuts-set-4-init

---
+++ S1D3Python/pythonSet4.py
@@ -0,0 +1,22 @@
+#1. **Anagram Check**: Write a Python function that checks whether two given words are anagrams.
+    # - *Input*: "cinema", "iceman"
+    # - *Output*: "True"
+
+
+
+
+
+# 2. **Bubble Sort**: Implement the bubble sort algorithm in Python.
+#     - *Input*: [64, 34, 25, 12, 22, 11, 90]
+#     - *Output*: "[11, 12, 22, 25, 34, 64, 90]"
+
+
+
+# 3. **Longest Common Prefix**: Given a list of strings, find the longest common prefix.
+    # - *Input*: ["flower","flow","flight"]
+    # - *Output*: "fl"
+
+
+# 4. **String Permutations**: Write a Python function to calculate all permutations of a given string.
+    # - *Input*: "abc"
+    # - *Output*: "['abc', 'acb', 'bac', 'bca', 'cab', 'cba']"
