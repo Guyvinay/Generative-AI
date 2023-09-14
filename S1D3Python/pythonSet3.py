5be5a6ccc8eeeadb3a76457c7da1d10b1efab5b4
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-14 18:31:50 +0530

    python-basic-tuts-set31comp

---
+++ S1D3Python/pythonSet3.py
@@ -0,0 +1,8 @@
+# 1. **Tuple Unpacking**: Create a list of tuples, each containing a name and an age. Then, use tuple unpacking to iterate through the list and print each name and age.
+
+
+    # - *Output*: "John is 25 years old. Jane is 30 years old."
+
+input= [("John", 25), ("Jane", 30)]
+
+print(f"{input[0][0]} is {input[0][1]}. {input[1][0]} is {input[1][1]}") 
