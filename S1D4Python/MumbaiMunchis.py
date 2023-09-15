cc0a83f9f316335c668d6087c9cbc2b479fa9d97
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-15 16:11:31 +0530

    Mumbai-munchis-view-all0snacks-impl

--- S1D4Python/MumbaiMunchis.py
+++ S1D4Python/MumbaiMunchis.py
@@ -1,4 +1,4 @@
-snackInventory={}
+'''snackInventory={}
 salesRecords={}
 
 def addSnak():
@@ -70,4 +70,127 @@ while True:
     elif choice == "5":
         break
     else:
+        print("Invalid choice. Please choose a valid option.")'''
+
+        # Initialize the snack inventory as a dictionary
+snack_inventory = {}
+
+# Initialize the sales record as a dictionary
+sales_record = {}
+
+# Function to add a snack to the inventory
+def add_snack():
+    snack_id = input("Enter snack ID: ")
+    snack_name = input("Enter snack name: ")
+    price = float(input("Enter snack price: "))
+    available = input("Is the snack available? (yes/no): ").lower()
+    
+    snack_inventory[snack_id] = {
+        "name": snack_name,
+        "price": price,
+        "available": available == "yes",
+    }
+    print(f"Snack '{snack_name}' added to the inventory.")
+
+# Function to remove a snack from the inventory
+def remove_snack():
+    snack_id = input("Enter snack ID to remove: ")
+    if snack_id in snack_inventory:
+        del snack_inventory[snack_id]
+        print(f"Snack with ID '{snack_id}' removed from the inventory.")
+    else:
+        print(f"Snack with ID '{snack_id}' not found in the inventory.")
+
+# Function to update the availability of a snack
+def update_availability():
+    snack_id = input("Enter snack ID to update availability: ")
+    if snack_id in snack_inventory:
+        available = input("Is the snack available? (yes/no): ").lower()
+        snack_inventory[snack_id]["available"] = available == "yes"
+        print(f"Availability of snack '{snack_inventory[snack_id]['name']}' updated.")
+    else:
+        print(f"Snack with ID '{snack_id}' not found in the inventory.")
+
+# Function to record a sale
+def record_sale():
+    snack_id = input("Enter snack ID sold: ")
+    if snack_id in snack_inventory:
+        if snack_inventory[snack_id]["available"]:
+            quantity = int(input("Enter quantity sold: "))
+            if snack_id in sales_record:
+                sales_record[snack_id] += quantity
+            else:
+                sales_record[snack_id] = quantity
+            print(f"Sale recorded for snack '{snack_inventory[snack_id]['name']}'.")
+            snack_inventory[snack_id]["available"] = False
+        else:
+            print(f"Snack '{snack_inventory[snack_id]['name']}' is not available.")
+    else:
+        print(f"Snack with ID '{snack_id}' not found in the inventory.")
+
+# Function to view all snacks in the inventory
+def view_all_snacks():
+    if not snack_inventory:
+        print("The inventory is empty.")
+    else:
+        print("Snack ID  |  Snack Name  |  Price  |  Availability")
+        print("-" * 50)
+        for snack_id, snack_info in snack_inventory.items():
+            print(f"{snack_id:<10} | {snack_info['name']:<12} | {snack_info['price']:<6.2f} | {'Yes' if snack_info['available'] else 'No'}")
+
+# Main loop for user interaction
+while True:
+    print("\nMumbai Munchies Inventory Management System")
+    print("1. Add a snack to the inventory")
+    print("2. Remove a snack from the inventory")
+    print("3. Update snack availability")
+    print("4. Record a sale")
+    print("5. View all snacks")
+    print("6. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        add_snack()
+    elif choice == "2":
+        remove_snack()
+    elif choice == "3":
+        update_availability()
+    elif choice == "4":
+        record_sale()
+    elif choice == "5":
+        view_all_snacks()
+    elif choice == "6":
+        break
+    else:
         print("Invalid choice. Please choose a valid option.")
+
+
+
+
+# # Main loop for user interaction
+# while True:
+#     print("\nMumbai Munchies Inventory Management System")
+#     print("1. Add a snack to the inventory")
+#     print("2. Remove a snack from the inventory")
+#     print("3. Update snack availability")
+#     print("4. Record a sale")
+#     print("5. View All Snacks")
+#     print("6. Exit")
+    
+#     choice = input("Enter your choice: ")
+    
+#     if choice == "1":
+#         add_snack()
+#     elif choice == "2":
+#         remove_snack()
+#     elif choice == "3":
+#         update_availability()
+#     elif choice == "4":
+#         record_sale()
+#     elif choice == "5":
+#         view_snacks()
+#     elif choice == "6":
+#         break
+#     else:
+#         print("Invalid choice. Please choose a valid option.")
