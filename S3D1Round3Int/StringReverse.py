445a8316bc95e7de7a4a96025a858756ac365d4c
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-25 17:44:54 +0530

    String-reverse-problem-comp

--- S3D1Round3Int/StringReverse.py
+++ S3D1Round3Int/StringReverse.py
@@ -1,6 +1,11 @@
 
 def stringReverse():
-    inputStr = input("Enter Any String You want to Reverse!")
-    return inputStr
+    inputStr = input("Enter Any String You want to Reverse!:- ")
+    str=inputStr[::-1]
+    
+    # for char in inputStr:
+    #     str+=char
+    return str
+
 
 print(stringReverse())
