f7a30bca200342804d0021442dc4c27e9288307e
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-25 23:36:59 +0530

    update-getall-method-integrated

--- S3D1Round3Int/FacebookFlaskApp/app.py
+++ S3D1Round3Int/FacebookFlaskApp/app.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 app = Flask(__name__)
-
 post = {}
-
-@app.route('/post')
+@app.route('/posts')
 def postCreation():
     postKye = input("Enter Your UserName:- ")
     postValue = input("Enter Caption for your Post:- ")
@@ -12,6 +9,16 @@ def postCreation():
     print(post[postKye])
     return f"SuccessFully Created {post[postKye]} "
 
+@app.route('/posts/delete/<username>')
+def delete_creation(username):
+    if username in post:
+        del post[username]
+        return f"{username} Deleted Successfully! "
+    return f"{username} not found! "
 
+@app.route("/getposts")
+def get_app_posts():
+    return jsonify(post)
+    
 if __name__=='__main__':
     app.run(debug=True)
