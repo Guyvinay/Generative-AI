245d2eb73be924b013a1dedebc19d7a5fbdc5f63
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-28 13:29:41 +0530

    zomato-chronicals-integrating-with-mongoDB-add-dish-update-availabilty-comp

--- S3D2ZomatoChroMongo/app.py
+++ S3D2ZomatoChroMongo/app.py
@@ -10,6 +10,7 @@ db = client["mydb"]
 menu_collection = db["menu"]
 order_collection = db["orders"]
 
+# Function to add a new dish to the menu
 @app.route('/add_dish',methods=['POST'])
 def addNewDish():
     data = request.json
@@ -29,6 +30,30 @@ def addNewDish():
         "menu_item":f"{menu_item}"
     }
     return jsonify(response_data)
+
+# Route to remove a dish from the menu
+@app.route('/remove_dish/<dish_id>',methods=['DELETE','GET'])
+def remove_dish(dish_id) : 
+    result = menu_collection.delete_one({"dish_id":dish_id})
+    if result.deleted_count == 1 :
+        return jsonify({"message":"Dish removed from the menu.."}),201
+    else :
+        return jsonify({"message":"Dish not found in the menu.."}),201
+
+
+# Route to update dish availability
+@app.route('/update_availability/<dish_id>',methods=['PUT'])
+def update_availability(dish_id) :
+    data = request.json
+    available = data.get('available')
+    result = menu_collection.update_one(
+        {"dish_id":dish_id},
+        {"$set":{"available":available}}
+    )
+    if result.modified_count == 1 :
+        return jsonify({"message": f"Availability for dish {dish_id} updated to  {available} ."}), 200
+    else :
+        return jsonify({"message": "Dish not found in the menu."}), 404
 
 
 
